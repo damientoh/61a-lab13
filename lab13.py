8ab9e9cb130b1dd4d3f5bd029463aae2fc007c4e
Damien Toh <personal@damienone.com>
2022-04-21 19:41:34 -0700

    finish

--- lab13.py
+++ lab13.py
@@ -49,7 +49,7 @@ def calculator_ops(calc_str):
     >>> calculator_ops("+ 3 23")
     False
     """
-    return bool(re.search(__________, calc_str))
+    return bool(re.search(r'\([+-/*]\s\d\s\d\)', calc_str))
 
 
 import re
@@ -73,4 +73,4 @@ def roman_numerals(text):
     >>> roman_numerals("she loves ALL editors equally.")
     False
     """
-    return bool(re.search(__________, text))
+    return bool(re.search(r'\b[I,V,X,L,C,D,M]+\b', text))
